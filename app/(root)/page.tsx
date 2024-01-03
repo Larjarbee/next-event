d3ea401c58ec28525cfd4fa0e5e61bd061e47b34
osunlaja abiodun <osunlajajamiu97@gmail.com>
2024-01-03 21:43:24 +0100

    added event update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,23 @@
-// import CategoryFilter from '@/components/shared/CategoryFilter';
-// import Collection from '@/components/shared/Collection';
-// import Search from '@/components/shared/Search';
-// import { getAllEvents } from '@/lib/actions/event.actions';
-// import { SearchParamProps } from '@/types';
+// import CategoryFilter from '@/components/common/CategoryFilter';
+import Collection from '@/components/common/Collection';
+// import Search from '@/components/common/Search';
+import { getAllEvents } from '@/lib/actions/event.actions';
+import { SearchParamProps } from '@/types';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
+
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6,
+  });
   return (
     <>
       <section className='bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10'>
@@ -48,7 +58,7 @@ export default function Home() {
           <CategoryFilter />
         </div> */}
 
-        {/* <Collection
+        <Collection
           data={events?.data}
           emptyTitle='No Events Found'
           emptyStateSubtext='Come back later'
@@ -56,7 +66,7 @@ export default function Home() {
           limit={6}
           page={page}
           totalPages={events?.totalPages}
-        /> */}
+        />
       </section>
     </>
   );
